12a5c68f5fa3352237930531515a5d95c1a08de4
Robert M. Thomson <rmt-git@corporatism.org>
2010-05-30 15:40:21 +0200

    Initial commit

---
+++ navix.py
@@ -0,0 +1,355 @@
+#!/usr/bin/python
+
+import re
+import sys
+import cmd
+import cPickle as pickle
+import os.path
+import time
+import urllib, urllib2
+from pprint import pprint
+from subprocess import Popen, PIPE
+from fnmatch import fnmatch
+from cookielib import CookieJar
+import textwrap
+#
+import scraper
+
+DOWNLOADDIR = os.path.join(os.environ['HOME'], 'Download')
+
+class myURLOpener(urllib.FancyURLopener):
+    def http_error_206(self, url, fp, errcode, errmsg, headers, data=None):
+        pass
+myUrlClass = myURLOpener()
+
+cookiejar = CookieJar()
+
+USER_AGENT="Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2b5) Gecko/20091204 Firefox/3.6b5"
+
+def request(url, referer=None, ua=USER_AGENT, data=None, **kwargs):
+    d = { "User-Agent" : ua }
+    if referer:
+        d['Referer'] = referer
+    d.update(kwargs)
+    r = urllib2.Request(url, data, d)
+    return r
+def get(url, *args, **kwargs):
+    req = request(url, *args, **kwargs)
+    cookiejar.add_cookie_header(req)
+    print "Requesting %r" % url
+    #res = urllib2.urlopen(req)
+    res = myUrlClass.open(req)
+    cookiejar.extract_cookies(res, req)
+    return res
+def get_match(regex, content, num=1):
+    m = re.search(regex, content, re.I)
+    try:
+        return m.group(num)
+    except:
+        return None
+
+def download(res, filename):
+    length = res.info().get('Content-Length', None)
+    strlength = length and ("%dk" % (int(length)/1024)) or "Unknown"
+    i = 0
+    data = res.read(4096)
+    count = len(data)
+    out = None
+    while data:
+        if out is None:
+            fname = filename
+            i = 1
+            if res.getcode() == 206: # partial file transfer
+                # FIXME - seek to right location, in-case
+                out = file(fname, "ab")
+            else:
+                while os.path.exists(fname):
+                    fname = "%s.%d" % (fname, i)
+                    i = i + 1
+                out = file(fname, "wb")
+            print "Downloading to %s" % fname
+        out.write(data)
+        data = res.read(4096)
+        count += len(data)
+        sys.stdout.write("\r\033[K[%dk / %s]" % (count//1024, strlength))
+        sys.stdout.flush()
+    out.close()
+    print ""
+
+class Cache(object):
+    def __init__(self, filename):
+        if os.path.exists(filename):
+            self.data = pickle.load(file(filename,'rb'))
+        else:
+            self.data = {}
+        self.filename = filename
+    def __setitem__(self, k, v):
+        self.data[k] = (time.time(), v)
+    def __getitem__(self, k):
+        tm, v = self.data[k]
+        if (time.time()-tm) > 3600:
+            del self.data[k]
+            raise KeyError()
+        return v
+    def get(self, k, default=None):
+        try:
+            return self.data[k]
+        except KeyError:
+            return default
+    def __contains__(self, k):
+        if k in self.data:
+            tm = self.data[k][0]
+            if (time.time()-tm) > 3550:
+                return True
+        return False
+    def save(self):
+        pickle.dump(self.data, file(self.filename,'wb'))
+
+def parse_pls(url):
+    import re
+    fd = urllib2.urlopen(url)
+    d = {}
+    indesc = False
+    for line in fd:
+        line = line.strip()
+        if indesc:
+            if line.endswith("/description"):
+                indesc = False
+            line = line[:-12]
+            d['description'] += '\n' + line
+            continue
+        if re.search("^#?$", line):
+            if d and 'type' in d:
+                yield d
+            d = {}
+            continue
+        if line.startswith('#'):
+            continue
+        if '=' in line:
+            k,v = line.split('=', 1)
+            if k == 'description':
+                if v.endswith("/description"):
+                    v = v[:-12]
+                else:
+                    indesc = True
+            d[k] = v
+            continue
+    if d and 'type' in d:
+        yield d
+
+class Item(dict):
+    def __init__(self, d):
+        dict.__init__(self, d)
+        self._v = None
+    @property
+    def v(self):
+        if self._v:
+            return self._v
+        if self['type'] == 'playlist':
+            self._v = Playlist(self['URL'])
+            return self._v
+        if self['type'] == 'video':
+            self._v = Video(self)
+            return self._v
+        if self['type'] == 'audio':
+            self._v = Audio(self)
+            return self._v
+
+class Video(dict):
+    def __str__(self):
+        return 'Video(%s)' % (self.get('name', None)
+            or self.get('URL',None) or hex(id(self)))
+    def __repr__(self):
+        return '<Video %s>' % (self.get('name', None)
+            or self.get('URL',None) or hex(id(self)))
+
+class Audio(dict):
+    def __str__(self):
+        return 'Audio(%s)' % (self.get('name', None)
+            or self.get('URL',None) or hex(id(self)))
+    def __repr__(self):
+        return '<Audio %s>' % (self.get('name', None)
+            or self.get('URL',None) or hex(id(self)))
+
+class Playlist(list):
+    def __init__(self, url):
+        self.url = url
+        self.d = d = {}
+        for x in parse_pls(url):
+            i = Item(x)
+            if 'URL' in x:
+                d[x['URL']] = i
+            self.append(i)
+
+class BaseCmd(cmd.Cmd):
+    def do_EOF(self, line=None):
+        print ""
+        return True
+    def emptyline(self):
+        pass
+
+class PlaylistCmd(BaseCmd):
+    def __init__(self, name, playlist, *args, **kwargs):
+        name = re.sub('\[\/?COLOR.*?\]','', name)
+        self.prompt = name + "> "
+        self.playlist = playlist
+        BaseCmd.__init__(self, *args, **kwargs)
+    def do_info(self, line):
+        d = self._getd(line)
+        if d is None:
+            print "!! Cannot find %s" % line
+            return
+        if 'name' in d:
+            for x in textwrap.wrap(d['name'], 70):
+                print x
+            print '-'*min(len(d['name']), 70)
+        if 'description' in d:
+            for l in d['description'].decode('utf-8').split('\n'):
+                for x in textwrap.wrap(l):
+                    if type(x) == unicode:
+                        print x.encode('utf-8')
+                    else:
+                        try: print x.decode('utf-8')
+                        except: print x.decode('iso-8859-1', 'replace')
+        if 'URL' in d:
+            print '[URL=%s]' % d['URL']
+    do_show = do_info
+    def do_list(self, line):
+        line = line.strip()
+        i = -1
+        types = { 'playlist' : 'pls', 'vid' : 'vid' }
+        pipe = Popen(["more"], stdin=PIPE)
+        for x in self.playlist:
+            i += 1
+            name = re.sub('\[\/?COLOR.*?\]','', x['name'])
+            if line and not fnmatch(name, line):
+                continue
+            typ = types.get(x['type'], None) or x['type']
+            pipe.stdin.write("[%-3d] (%s) %s\n" % (i, typ, name))
+        pipe.stdin.close()
+        pipe.wait()
+    do_ls = do_list
+    def do_cd(self, line):
+        if line == "..":
+            return True
+        d = self._getd(line)
+        if d is None:
+            print "!! Cannot cd to %s" % line
+            return
+        if d['type'] == 'playlist':
+            pl = PlaylistCmd(d['name'], d.v)
+            pl.onecmd("ls")
+            pl.cmdloop()
+        else:
+            print "!! Cannot cd to %s" % line
+    def do_more(self, line):
+        d = self._getd(line)
+        if 'URL' in d and 'type' in d:
+            if d['type'] in ('video', 'audio'):
+                print "!! Cannot view binary data as a text file"
+                return
+            req = request(d['URL'])
+            g = urllib2.urlopen(req)
+            pipe = Popen(["more"], stdin=PIPE)
+            while True:
+                b = g.read(512)
+                if not b:
+                    break
+                try:
+                    pipe.stdin.write(b)
+                except IOError:
+                    break
+            pipe.stdin.close()
+            pipe.wait()
+            print ""
+    def do_search(self, line):
+        pl = Playlist("http://navix.turner3d.net/playlist/search/%s" % (
+            urllib.quote_plus(line)))
+        if len(pl) > 0:
+            pc=PlaylistCmd("Results for '%s'" % line, pl)
+            pc.onecmd("ls")
+            pc.cmdloop()
+        else:
+            print "No results for '%s'" % line
+    def do_dump(self, line):
+        d = self._getd(line)
+        if d is None:
+            print "!! Cannot show %s" % line
+            return
+        pprint(d)
+    def _getd(self, line):
+        try:
+            return self.playlist[int(line.strip())]
+        except:
+            return None
+
+    def do_proc(self, line):
+        d = self._getd(line)
+        if 'processor' in d:
+            purl = "%s?url=%s" % (d['processor'], urllib.quote(d['URL']))
+            print "Processing with %s" % purl
+            print urllib2.urlopen(purl).read()
+            print
+        else:
+            print "No processor required for", d['URL']
+
+    def do_get(self, line):
+        fname = None
+        if re.search('\d+ to .+', line):
+            line, fname = line.split(' to ', 1)
+        d = self._getd(line)
+        if 'URL' in d and 'name' in d:
+            if fname:
+                if '/' not in fname:
+                    fname = os.path.join(DOWNLOADDIR, fname)
+            else:
+                fname = d['name']
+                fname = fname.rsplit("/",1)[-1].replace(" ","_") + ".avi"
+                fname = os.path.join(DOWNLOADDIR, fname)
+            if os.path.exists(fname):
+                byterange = "Range: bytes=%s-" % (os.path.getsize(fname)+1)
+            else:
+                byterange = None
+            try:
+                if 'processor' in d:
+                    res = scraper.navix_get(d['processor'], d['URL'], byterange=byterange, verbose=1)
+                else:
+                    if byterange:
+                        res = get(d['URL'], Range=byterange)
+                    else:
+                        res = get(d['URL'])
+                download(res, fname)
+            except:
+                import traceback
+                traceback.print_exc()
+
+    def do_play(self, line):
+        d = self._getd(line)
+        res = None
+        if 'processor' in d and 'URL' in d:
+            res = scraper.navix_get(d['processor'], d['URL'], verbose=0)
+        elif 'URL' in d:
+            res = urllib.urlopen(request(d['URL']))
+        if res:
+            mplayer = Popen(['mplayer', '-cache-min', '5', '-cache', '102400' '-'], stdin=PIPE)
+            while True:
+                bytes = res.read(16384)
+                if not bytes:
+                    break
+                try:
+                    mplayer.stdin.write(bytes)
+                except:
+                    res.close()
+                    break
+            mplayer.stdin.close()
+        else:
+            print "Missing some info"
+
+    def do_reload(self, line):
+        reload(scraper)
+
+if __name__ == '__main__':
+    #cache = Cache("playlist.pickle")
+    pl = Playlist("http://navix.turner3d.net/playlist/index.plx")
+    plc = PlaylistCmd("index", pl)
+    plc.cmdloop()
---
+++ scraper.py
@@ -0,0 +1,364 @@
+#!/usr/bin/python
+
+import re
+import sys
+import time
+import urllib
+import urllib2
+import os.path
+import hashlib
+import cookielib
+from urllib import quote, quote_plus, unquote
+
+USER_AGENT="Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2b5) Gecko/20091204 Firefox/3.6b5"
+
+def get_match(regex, content, num=1):
+    m = re.search(regex, content, re.I)
+    try:
+        return m.group(num)
+    except:
+        return None
+
+class Browser(object):
+    def __init__(self, ua=USER_AGENT, refpolicy=0, headers=None):
+        self.user_agent = ua
+        self.cookiejar = cookielib.CookieJar()
+        self.headers = headers or {}
+        self.refpolicy = 0
+    def make_request(self, url, referer=None, ua=USER_AGENT, data=None,
+                     cookies=None, **kwargs):
+        d = { "User-Agent" : self.user_agent }
+        d.update(self.headers)
+        if referer:
+            d['Referer'] = referer
+        d.update(kwargs)
+        r = urllib2.Request(url, data, d)
+        #if type(cookies) == dict:
+        #    c = Cookie()
+        #    for k,v in cookies.items():
+        self.cookiejar.add_cookie_header(r)
+        return r
+    def get(self, url, *args, **kwargs):
+        req = self.make_request(url, *args, **kwargs)
+        res = urllib2.urlopen(req)
+        #print "Requested %s" % url
+        self.cookiejar.extract_cookies(res, req)
+        return res
+    def add_cookie(cookie):
+        pass
+
+def navix_get(procurl, url, browser=None, _ttl=5, byterange=None, verbose=0):
+        if not _ttl:
+            print "In a loop!"
+            return None
+        if browser is None:
+            browser = Browser()
+        if not isinstance(browser, Browser):
+            print `browser`
+        if url.startswith("http://") or url.startswith("https://"):
+            gurl = "%s?url=%s" % (procurl, quote_plus(url))
+        else: # pre-quoted
+            gurl = "%s?%s" % (procurl, url)
+        if verbose:
+            print "Fetching %r" % gurl
+        htmRaw = browser.get(gurl).read()
+        proc = htmRaw.splitlines()
+        if not proc:
+            return None
+        if not proc[0].startswith("v2"):
+            if verbose:
+                print "Fetching %r" % proc[0]
+            if byterange is not None:
+                v1 = browser.get(proc[0], Range=byterange)
+            else:
+                v1 = browser.get(proc[0])
+            if len(proc) == 1:
+                return v1 # the final url
+            m = re.search(proc[1], v1.read())
+            i = 0
+            parts = []
+            for g in m.groups():
+                i += 1
+                parts.append("v%s=%s" % (i, quote_plus(g)))
+            return navix_get(procurl, "&".join(parts), browser, _ttl=_ttl-1, byterange=byterange)
+        #
+        # v2 script: a DSL for scraping webpages
+        # http://navix.turner3d.net/proc_docs/
+        #
+        proc = proc[1:]
+        htmRaw = htmRaw[3:]
+        inst = htmRaw
+        phase = 0
+        exflag = False
+        phase1complete = False
+        proc_args = ''
+        inst_prev = ''
+        def_agent='Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.4) Gecko/2008102920 Firefox/3.0.4'
+        #
+        v_defaults = {
+                'htmRaw':'',
+                's_url':'',
+                'regex':'',
+                's_method':'get',
+                's_action':'read',
+                's_agent':def_agent,
+                's_referer':'',
+                's_cookie':'',
+                's_postargs':'',
+                'url':'',
+                'swfplayer':'',
+                'playpath':'',
+                'agent':'',
+                'pageurl':''
+        }
+        v = v_defaults.copy()
+        # command parser
+        lparse=re.compile('^([^ =]+)([ =])(.+)$')
+        # condition parser
+        ifparse=re.compile('^([^<>=!]+)\s*([!<>=]+)\s*(.+)$');
+        while exflag == False:
+            scrape = 1
+            phase = phase + 1
+            rep = {}
+
+            if_satisfied = False
+            if_next = False
+            if_end = False
+
+            src_printed = False
+
+            if proc_args:
+                inst = browser.get(procurl+"?"+proc_args)
+                proc_args = ''
+            elif phase1complete:
+                exflag = True
+            else:
+                v['s_url'] = url
+
+            if inst == inst_prev:
+                print "Endless loop detected"
+                return None
+
+            inst_prev = inst
+            lines = inst.splitlines()
+            if not len(lines):
+                print "Processor error: nothing returned from phase "+phase
+                return None
+
+            linenum = 0
+            for line in lines:
+                #print "Processing %r" % line
+                linenum += 1
+                line = re.sub('^\s*', '', line)
+                if verbose > 0 and src_printed == False:
+                    print "Processor NIPL source:\n"+inst
+                    src_printed = True
+
+                if line[:1] == '#' or not line:
+                    continue
+
+                if if_end and line != 'endif':
+                    continue
+
+                if if_next and line[:5] != 'elseif' and line != 'else' and line != 'endif':
+                    continue
+
+                if line == 'else':
+                    if if_satisfied:
+                        if_end=True
+                    else:
+                        if_next = False
+                    continue
+                # /else
+
+                elif line == 'endif':
+                    if_satisfied = False
+                    if_next = False
+                    if_end = False
+                    continue
+                # /endif
+
+                elif line == 'scrape':
+                    if not v['s_url']:
+                        return None
+                    if verbose:
+                        print "Scraping %r" % v['s_url']
+                    scrape = scrape + 1
+                    if v['s_method'] == 'get':
+                        kwargs = {}
+                        if v.get('s_cookie',''):
+                            kwargs['Cookie'] = v['s_cookie']
+                        v['htmRaw'] = browser.get(v['s_url'], referer=v['s_referer'], **kwargs)
+                    elif v['s_method'] == 'post':
+                        kwargs = {}
+                        if v.get('s_cookie',''):
+                            kwargs['Cookie'] = v['s_cookie']
+                        res = browser.get(v['s_url'], referer=v['s_referer'], data=v['s_postdata'], **kwargs)
+                        if v['s_action'] == 'read':
+                            v['htmRaw'] = res.read()
+                        elif v['s_action'] == 'geturl':
+                            v['v1'] = res.geturl()
+                        res.close()
+                    if v['s_action'] == 'read' and v['regex'] > '':
+                        v['nomatch'] = ''
+                        rep['nomatch'] = ''
+                        for i in xrange(1, 11):
+                            ke = 'v'+str(i)
+                            v[ke] = ''
+                            rep[ke] = ''
+                        p = re.compile(v['regex'])
+                        match = p.search(v['htmRaw'])
+                        if match:
+                            for i in xrange(1, len(match.groups())+1):
+                                val = match.group(i)
+                                key='v'+str(i)
+                                rep[key] = val
+                                v[key] = val
+                        else:
+                            print "Processor scrape: no match"
+                            rep['nomatch'] = 1
+                            v['nomatch'] = 1
+                # /scrape
+
+                elif line == 'play':
+                    exflag = True
+                # /play
+
+                elif line == 'report':
+                    rep['phase'] = str(phase)
+                    proc_args = urllib.urlencode(rep)
+                    proc_args = re.sub('v\d+=&', '&', proc_args)
+                    proc_args = proc_args.replace('nomatch=&', '&')
+                    proc_args=re.sub('&+','&',proc_args)
+                    proc_args=re.sub('^&','',proc_args)
+                # /report
+
+                else:
+                    # parse
+                    match = lparse.search(line)
+                    if match is None:
+                        print "Processor syntax error: "+line
+                        return None
+                    subj = match.group(1)
+                    arg = match.group(3)
+
+                    if subj == 'if' or subj == 'elsif':
+                        if if_satisfied:
+                            if_end = True
+                        else:
+                            # process if with operators
+                            match = ifparse.search(arg)
+                            if match:
+                                lkey = match.group(1)
+                                oper = match.group(2)
+                                rraw = match.group(3)
+                                if oper == '=':
+                                    oper = '=='
+                                if lkey not in v:
+                                    v[lkey] = ''
+                                if rraw[0:1] == "'":
+                                    rside = rraw[1:]
+                                else:
+                                    if rraw not in v:
+                                        v[rraw] = ''
+                                    rside = v[rraw]
+                                _bool = eval("v[lkey]" + oper + "rside")
+                            else:
+                                # process single if argument
+                                if arg not in v:
+                                    v[arg] = ''
+                                _bool = bool(v[arg])
+                        if _bool:
+                            if_satisfied = True
+                            if_next = False
+                        else:
+                            if_next = True
+                        continue
+                    if match.group(2) == '=':
+                        # assignment operator
+                        if arg[0:1] == "'":
+                            v[subj]=arg[1:]
+                        else:
+                            if arg not in v:
+                                v[arg] = ''
+                            v[subj] = v[arg]
+                    else:
+                        # do command
+                        if subj == 'verbose':
+                            try: verbose = int(arg)
+                            except: verbose = 0
+                        elif subj == 'error':
+                            print "Processing error: "+arg[1:]
+                            return
+                        elif subj == 'report_val':
+                            match = lparse.search(arg)
+                            if match is None:
+                                print "Processor syntax error: "+line
+                                return
+                            ke = match.group(1)
+                            va = match.group(3)
+                            if va[0:1] == "'":
+                                rep[ke] = va[1:]
+                            else:
+                                rep[ke] = v.get(va,'')
+                        elif subj=='concat':
+                            match = lparse.search(arg)
+                            if match is None:
+                                print "Processor syntax error: "+line
+                                return
+                            ke = match.group(1)
+                            va = match.group(3)
+                            oldtmp = v.get(ke,'')
+                            if va[0:1] == "'":
+                                v[ke] = v[ke] + va[1:]
+                            else:
+                                v[ke] = v[ke] + v.get(va,'')
+                        elif subj == 'match':
+                            v['nomatch'] = ''
+                            rep['nomatch'] = ''
+                            for i in xrange(1,11):
+                                ke = 'v'+str(i)
+                                v[ke] = ''
+                                rep[ke] = ''
+                            p = re.compile(v['regex'])
+                            match = p.search(v[arg])
+                            if match:
+                                for i in xrange(1, len(match.group())+1):
+                                    v['v%d'%i] = match.group(i)
+                            else:
+                                v['nomatch'] = 1
+                        elif subj == 'replace':
+                            # preset regex, replace var [']val
+                            match = lparse.search(arg)
+                            if match is None:
+                                print "Processor syntax error: "+line
+                                return
+                            ke = match.group(1)
+                            va = match.group(3)
+                            if va[0:1] == "'":
+                                va=va[1:]
+                            else:
+                                va=v.get(va, '')
+                            oldtmp = v.get(ke, '') # ??
+                            v[ke] = re.sub(v['regex'], va, v[ke])
+                        elif subj == 'unescape':
+                            oldtmp = v.get(arg, '')
+                            v[arg] = urllib.unquote(oldtmp)
+                        elif subj == 'debug' and verbose > 0:
+                            print "Processor debug "+arg+":\n" + v.get(arg,'');
+                        elif subj == 'print':
+                            if arg[0:1] == "'":
+                                print "Processor print: "+arg[1:]
+                            else:
+                                print "Processor print: "+v.get(arg,'')
+                        else:
+                            print "Processor error: unrecognised method '%s'" % subj
+
+            kwargs = {}
+            if v.get('s_cookie'):
+                kwargs['Cookie'] = v['s_cookie']
+            if byterange is not None:
+                kwargs['Range'] = byterange
+            print "URL: %s" % v.get('url','')
+            if v.get('url',''):
+                return browser.get(v['url'], **kwargs)
